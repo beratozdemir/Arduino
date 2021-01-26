a91c856ef198817dbfa32517bb08bbb43bcd7f44
BOSoft <50171097+beratozdemir@users.noreply.github.com>
2021-01-26 11:00:37 +0300

    Countdown_Timer
    Project Closed

--- Countdown_Timer.ino
+++ Countdown_Timer.ino
@@ -15,7 +15,7 @@ void setup() {
   display.setBacklight(100);  // set the brightness to 100 %
   delay(1000);                // wait 1000 ms 
   pinMode(7, OUTPUT);
-  };
+  }
 
 void loop() {
   byte hours    = 2;                           // initialize hours
@@ -24,7 +24,7 @@ void loop() {
   for ( ; hours < 3; hours-- ) {                // count hours   up to 24
     for (; minutes < 60; minutes--) {          // count minutes up to 59
       display.printTime(hours, minutes, true);  // display time
-      delay(60000 / clockSpeed);   
+      delay(60000 / clockSpeed);  
 
 if (hours < 3) 
 digitalWrite(7, HIGH);
@@ -48,8 +48,8 @@ break;
 delay(50);
               }
 delay(100);             // clock delay ms
-    };
+    }
     minutes = 59;       // reset minutes
-  };
-};
+  }
+}
 
